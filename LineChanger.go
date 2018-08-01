b0ac653b77d709bd455b29c69a815b7c23d8c7b9
Sang-Yeon Jeon <jof4002@gmail.com>
2018-08-01 14:34:46 +0900

    일단 마무리

--- LineChanger.go
+++ LineChanger.go
@@ -5,7 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
+
+	"golang.org/x/text/encoding/korean"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
+
+// var (
+// 	euckrDec = korean.EUCKR.NewDecoder()
+// 	utf16Dec = unicode.U
+// )
 
 // LineConfig root of all config
 type LineConfig []LineConfigElement
@@ -20,23 +31,151 @@ type LineConfigElement struct {
 
 // Change find and change to
 type Change struct {
-	Find     string            `json:"find"`
-	Changeto map[string]string `json:"changeto"`
+	Find        string            `json:"find"`
+	Description string            `json:"description"`
+	Changeto    map[string]string `json:"changeto"`
+	findPrefix  string
+	findPostfix string
 }
 
-func processLine(lc LineConfigElement) {
-	//fmt.Printf("line : %+v\n", lc)
+func readFile(path, encoding string) ([]string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	if encoding == "euckr" {
+		euckrDec := korean.EUCKR.NewDecoder()
+		got, _, err := transform.Bytes(euckrDec, data)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+		lines := strings.Split(string(got), "\n")
+		return lines, nil
+	} else if encoding == "utf16bom" {
+		utfDec := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewDecoder()
+		got, _, err := transform.Bytes(utfDec, data)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+		lines := strings.Split(string(got), "\n")
+		return lines, nil
+	}
+	if encoding != "utf8" {
+		return nil, fmt.Errorf("unknown encoding %s", encoding)
+	}
+	lines := strings.Split(string(data), "\n")
+	return lines, nil
+}
+
+func writeFile(lines []string, path, encoding string) {
+	//fmt.Println("write : " + path)
+	sep := "\n"
+	onestring := strings.Join(lines, sep)
+	if encoding == "euckr" {
+		euckrEnc := korean.EUCKR.NewEncoder()
+		got, _, err := transform.Bytes(euckrEnc, []byte(onestring))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		ioutil.WriteFile(path, []byte(got), 0644)
+		return
+	}
+	if encoding == "utf16bom" {
+		utfEnc := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewEncoder()
+		got, _, err := transform.Bytes(utfEnc, []byte(onestring))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		ioutil.WriteFile(path, []byte(got), 0644)
+		return
+	}
+	ioutil.WriteFile(path, []byte(onestring), 0644)
+}
+
+func processItem(buildstage string, lc LineConfigElement, basePath string) {
+	// fmt.Println("-------------------------")
+	// fmt.Println("Path : " + basePath + lc.Path)
+	// fmt.Println("buildstage : " + buildstage)
+	// fmt.Println(lc.Description)
+
+	// preprocess find
+	for i, cc := range lc.Change {
+		arr := strings.Split(cc.Find, "[[tochange]]")
+		if len(arr) != 2 {
+			fmt.Println("invalid config Find format :" + cc.Find + " in " + lc.Path)
+			return
+		}
+		lc.Change[i].findPrefix = arr[0]
+		lc.Change[i].findPostfix = arr[1]
+	}
+
+	fileLines, err := readFile(basePath+lc.Path, lc.Encoding)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
 	for _, cc := range lc.Change {
-		for k, v := range cc.Changeto {
-			fmt.Println(k + " - " + v)
-		}
+		//fmt.Println(cc.Description)
 
+		for i, text := range fileLines {
+			//fmt.Printf("%d %s", i, text)
+			// for k, v := range cc.Changeto {
+			// 	fmt.Println(k + " - " + v)
+			// }
+			pre := strings.Index(text, cc.findPrefix)
+			if pre == -1 {
+				continue
+			}
+			if len(cc.findPostfix) > 0 && strings.Index(text[pre:], cc.findPostfix) == -1 {
+				continue
+			}
+
+			//fmt.Println("Found line : " + text)
+			first := text[:pre+len(cc.findPrefix)]
+			//fmt.Println("first token : " + first)
+			remain := text[pre+len(cc.findPrefix):]
+			second := ""
+			if len(cc.findPostfix) > 0 {
+				post := strings.Index(remain, cc.findPostfix)
+				if post != -1 {
+					second = remain[post:]
+					//fmt.Println("second token : " + second)
+				}
+			}
+
+			var replace string
+			if val, ok := cc.Changeto[buildstage]; ok {
+				replace = first + val + second
+			} else {
+				replace = first + second
+			}
+			fileLines[i] = replace
+		}
 	}
+
+	//writeFile(fileLines, basePath+lc.Path+".conv", lc.Encoding)
+	writeFile(fileLines, basePath+lc.Path, lc.Encoding)
 }
 
 func main() {
-	lineJSON, err := ioutil.ReadFile("example.json")
+	if len(os.Args) < 3 {
+		fmt.Println("LineChanger configjson buildstage (basepath)")
+		return
+	}
+	configjson := os.Args[1]
+	buildstage := os.Args[2]
+	basePath := "./"
+	if len(os.Args) >= 4 {
+		basePath = os.Args[3]
+	}
+
+	lineJSON, err := ioutil.ReadFile(configjson)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -48,8 +187,6 @@ func main() {
 		return
 	}
 	for _, line := range lines {
-		processLine(line)
+		processItem(buildstage, line, basePath)
 	}
-	//fmt.Printf("lines : %+v", lines)
-	//Birds : [{Species:pigeon Description:} {Species:eagle Description:bird of prey}]
 }
